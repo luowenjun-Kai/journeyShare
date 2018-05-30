ea5e0d212f22fcbf567c91a15935e8246c4e40bf
Vijay <765713272@qq.com>
2018-05-30 19:33:06 +0800

    add timeline in story page

--- src/components/story/story.js
+++ src/components/story/story.js
@@ -2,7 +2,28 @@ import './story.css'
 import pic from  '../../assets/homeimg.jpg'
 import React,{Component}from 'react';
 import {List} from 'antd';
+import {Timeline} from 'antd';
 export default class Story extends Component{
+    constructor(props){
+        super(props);
+        this.state=this.getInitialState();
+    }
+    getInitialState(){
+        return {
+            timeline:[
+                {time:'2017-5-5',des:'稻城'},
+                {time:'2016-5-5',des:'桂林'},
+                {time:'2014-1-28',des:'凤凰'},
+                {time:'2012-7-5',des:'成都'},
+            ]
+        }
+    }
+    getTimeLines(){
+        let lines=this.state.timeline.map((item)=>{
+            return <Timeline.Item><a href="#">{item.time} {item.des}</a></Timeline.Item>
+        })
+        return lines;
+    }
 
     render(){
         const data=[
@@ -24,7 +45,7 @@ export default class Story extends Component{
             <div id="body">
                 <div id="list">
                     <List itemLayout="vertical" dataSource={data} renderItem={item =>(
-                        <List.Item extra={<img width={272} alt="logo" src={pic} />}>
+                        <List.Item extra={<img width={200} alt="logo" src={pic} />}>
                             <List.Item.Meta title={item.title} description={item.describe}/>
                         </List.Item>
                     )}
@@ -32,7 +53,13 @@ export default class Story extends Component{
                     />
                 </div>
                 <div id='aside'>
-                    here I want to put some thing
+                    <h1>Time</h1>
+                    <div id="timeline">
+                        <Timeline>
+                            {this.getTimeLines()}
+                        </Timeline>
+                    </div>
+
                 </div>
             </div>
         )
