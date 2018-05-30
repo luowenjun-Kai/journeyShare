750769fad4318f5a4ecc8d24489db4788715462a
Vijay <765713272@qq.com>
2018-05-30 19:33:29 +0800

    make share page resiponsible

--- src/components/share/share.js
+++ src/components/share/share.js
@@ -2,44 +2,71 @@ import './share.css';
 import pic from '../../assets/homeimg.jpg';
 import React,{Component} from 'react';
 import { Row,Col} from 'antd';
+import { Collapse } from 'antd';
+const Panel=Collapse.Panel;
 export  default class Share extends Component{
+    constructor(props){
+        super(props);
+        this.state=this.getInitialState();
+    }
+    getInitialState(){
+        return {
+            journey:{
+                title:'深圳',
+                subtitle:'深圳野生动物园',
+                starttime:'2018-2-16',
+                money:600,
+                duration:6,
+                content:'something you want to share.something you want to share.something you want to share.something you want to share.something you want to share.something you want to share.something you want to share.something you want to share.something you want to share.',
+                img:[pic]
+            }
+        }
+    }
     render() {
         return (
             <div className="body">
-                <div className="msg">
+                <div className="title">
                     <Row>
-                        <Col lg={{span:8,offset:8}}>深圳</Col>
-                        <Col lg={{span:8,offset:12}}>深圳野生动物园</Col>
+                        <Col lg={{span:8,offset:8}}>{this.state.journey.title}</Col>
+                        <Col lg={{span:8,offset:12}}>{this.state.journey.subtitle}</Col>
                     </Row>
                 </div>
-                <div className="images">
-                    <Row className="hgt-4">
-                        <Col lg={{span:3}}><img src={pic}></img></Col>
-                        <Col lg={{span:3}}><img src={pic}></img></Col>
-
-                        <Col lg={{span:3}}><img src={pic}></img></Col>
-                        <Col lg={{span:3}}><img src={pic}></img></Col>
-                        <Col lg={{span:3}}><img src={pic}></img></Col>
-                        <Col lg={{span:3}}><img src={pic}></img></Col>
-                        <Col lg={{span:3}}><img src={pic}></img></Col>
-                        <Col lg={{span:3}}><img src={pic}></img></Col>
-                    </Row>
-                    <Row className="hgt-3">
-                        <Col lg={{span:8}}><img src={pic}></img></Col>
-                        <Col lg={{span:8}}><img src={pic}></img></Col>
-                        <Col lg={{span:8}}><img src={pic}></img></Col>
-                    </Row>
-                </div>
-                <div className="msg">
-                    <Row>
-                        <Col lg={{span:2,offset:18}}>2018-2-16</Col>
-                        <Col lg={{span:2}}>600元</Col>
-                        <Col lg={{span:2}}>6天</Col>
-                    </Row>
-                </div>
-                <div className="story">
-                    something you want to share.something you want to share.something you want to share.something you want to share.something you want to share.something you want to share.something you want to share.something you want to share.something you want to share.
-                </div>
+                <Row id="quick">
+                    <Col sm={{span:24}} lg={{span:12}} className="images">
+                        <Row gutter={8} className={"row"}>
+                            <Col span={8}  ><img src={this.state.journey.img[0]} alt={""}></img></Col>
+                            <Col span={8} ><img src={this.state.journey.img[0]} alt={""}></img></Col>
+                            <Col span={8}><img src={this.state.journey.img[0]} alt={""}></img></Col>
+                        </Row>
+                        <Row gutter={8} className={"row"}>
+                            <Col span={8}  ><img src={this.state.journey.img[0]} alt={""}></img></Col>
+                            <Col span={8} ><img src={this.state.journey.img[0]} alt={""}></img></Col>
+                            <Col span={8}><img src={this.state.journey.img[0]} alt={""}></img></Col>
+                        </Row>
+                        <Row gutter={8} className={"row"}>
+                            <Col span={8}  ><img src={this.state.journey.img[0]} alt={""}></img></Col>
+                            <Col span={8} ><img src={this.state.journey.img[0]} alt={""}></img></Col>
+                            <Col span={8}><img src={this.state.journey.img[0]} alt={""}></img></Col>
+                        </Row>
+                    </Col>
+                    <Col sm={{span:24}} lg={{span:12}} className="msg">
+                        <Collapse bordered={false} defaultActiveKey={['1']}>
+                            <Panel header={"概览"} key={"1"}>
+                                <Col lg={{span:5,offset:2}}>出发时间：{this.state.journey.starttime}</Col>
+                                <Col lg={{span:5}}>总花费：{this.state.journey.money}元</Col>
+                                <Col lg={{span:5}}>耗时：{this.state.journey.duration}天</Col>
+                            </Panel>
+                            <Panel header={"行程路线"} key={"2"}>
+                                <Col lg={{span:5,offset:2}}>1-2-3</Col>
+                            </Panel>
+                            <Panel header={"故事分享"} key={"3"}>
+                                <div>
+                                    {this.state.journey.content}
+                                </div>
+                            </Panel>
+                        </Collapse>
+                    </Col>
+                </Row>
 
 
             </div>
