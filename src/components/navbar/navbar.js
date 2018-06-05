370439dc6b80696e7a8c0cc6107b08a89249b244
Vijay <765713272@qq.com>
2018-06-05 15:05:29 +0800

    add navbar to fit sm size srcreen

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,30 +1,40 @@
 import './navbar.css';
 
 import React,{Component} from 'react';
-import { Menu} from 'antd';
+import { Menu,Icon} from 'antd';
 import { Col,Row } from "antd";
 import { Input } from "antd";
-//import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 //const SubMenu=Menu.SubMenu;
 const Search=Input.Search;
-
+const SubMenu = Menu.SubMenu;
 
 export default class Navbar extends Component {
     render() {
         return (
             <div>
                 <Row className="navbar">
-                    <Col span={8} className="logo">Logo</Col>
-                    <Col lg={{span:4,offset:2}} sm={{span:8}} className="search">
+                    <Col lg={{span:8}} sm={{span:0}} xs={{span:0}} className="logo">Logo
+                    </Col>
+                    <Col lg={{span:0}} sm={{span:8}} xs={{span:8}}>
+                        <Menu mode={"vertical"}>
+                            <SubMenu title={<span><Icon type={"menu-unfold"}/>Logo</span>}>
+                                <Menu.Item><a href="/">首页</a></Menu.Item>
+                                <Menu.Item><a href="/story">旅行故事</a></Menu.Item>
+                                <Menu.Item><a href="/share">照片分享</a></Menu.Item>
+                                <Menu.Item><a href="/support">支持</a></Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Col>
+                    <Col lg={{span:4,offset:2}} sm={{span:8}} xs={{span:10}} className="search">
                         <Search placeholder="搜索" onSearch={value => console.log(value)} enterButton/>
                     </Col>
                     <Col lg={{span:8,offset:2}} sm={{span:0}} xs={{span:0}}>
                         <Menu mode="horizontal">
                             <Menu.Item><a href="/">首页</a></Menu.Item>
-                            <Menu.Item><a href="footprint">地图足迹</a></Menu.Item>
-                            <Menu.Item><a href="story">旅行故事</a></Menu.Item>
-                            <Menu.Item><a href="share">照片分享</a></Menu.Item>
-                            <Menu.Item><a href="support">支持</a></Menu.Item>
+                            <Menu.Item><a href="/story">旅行故事</a></Menu.Item>
+                            <Menu.Item><a href="/share">照片分享</a></Menu.Item>
+                            <Menu.Item><a href="/support">支持</a></Menu.Item>
                         </Menu>
                     </Col>
                 </Row>
