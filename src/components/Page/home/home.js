ba45ba9fccec748602b975e1786c21851512b814
Vijay <765713272@qq.com>
2018-06-06 19:31:39 +0800

    user gapbar component

--- src/components/Page/home/home.js
+++ src/components/Page/home/home.js
@@ -1,11 +1,31 @@
+import './home.css'
 import React,{Component} from 'react';
 //引入所需组件
 import Footprint from '../../footprint/footprint';
+import Gapbar from '../../gapbar/gapbar'
 export default class HomePage extends Component{
+    gapbar=[
+        {
+            title:'About',
+            content:'Hello guys welcome to my website,Hello guys welcome to my website,Hello guys welcome to my website,Hello guys welcome to my website,Hello guys welcome to my website,Hello guys welcome to my website,Hello guys welcome to my website'
+        },
+        {
+            title:'Music',
+            content:'some music will be show '
+        }
+    ]
     render() {
         return (
             <div>
                 <Footprint></Footprint>
+                <div id={"home-body-wrap"}>
+                    <div id={'home-body'}>
+                        <Gapbar title={this.gapbar[0].title} content={this.gapbar[0].content} style={{background:'gapbarbk',titleColor:'#333631'}}/>
+                        <Gapbar title={this.gapbar[1].title} content={this.gapbar[1].content}/>
+                    </div>
+
+                </div>
+
             </div>
         )
 
--- src/components/Page/story/story.js
+++ src/components/Page/story/story.js
@@ -1,12 +1,13 @@
 import React,{ Component } from 'react';
 import Homeimg from '../../homeimg/homeimg';
 import Storylist from '../../storylist/storylist';
-
+import Gapbar from '../../gapbar/gapbar'
 export default class StoryPage extends Component{
     render(){
         return (
             <div>
                 <Homeimg/>
+                <Gapbar title={'往事'} content={""}/>
                 <Storylist/>
             </div>
         )
