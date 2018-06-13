54745cb61f0a793cb1c9cf055b456b5afd6f2687
Vijay <765713272@qq.com>
2018-06-13 19:19:28 +0800

    show journey record when user click specific area of map

--- src/components/Page/home/home.js
+++ src/components/Page/home/home.js
@@ -2,8 +2,11 @@ import './home.css'
 import React,{Component} from 'react';
 //引入所需组件
 import Footprint from '../../footprint/footprint';
-import Gapbar from '../../gapbar/gapbar'
+import Gapbar from '../../gapbar/gapbar';
+import Deslist from '../../deslist/deslist';
 import { Button } from 'antd'
+import axios from 'axios';
+import url from '../../../config';
 export default class HomePage extends Component{
     constructor(props){
         super(props);
@@ -11,23 +14,47 @@ export default class HomePage extends Component{
     }
     getInitialState(){
         return {
-            destination:''
+            destination:'',
+            rawdesdata:'',
         }
     }
     setDes(des){
-        this.setState({
-            destination:des
-        });
-        console.log("you choose the "+this.state.destination);
+        let cdes=this.state.destination;
+        console.log("you choose the "+des);
+        //根据des索引其下的目的地
+        axios.get(url.getJneyByDes + des).then((res)=>{
+            let data=res.data;
+           // console.log(data)
+            let showdata=[];
+            //处理数据传入组件
+            for(let i in data){
+                let desarr=data[i].destination.split('-');
+                let temp={
+                    journeyId:data[i].journeyId,
+                    time:new Date(data[i].deptime).toLocaleDateString(),
+                    des:desarr[1],
+                    cover:desarr[2]
+                }
+                showdata.push(temp)
+            }
+             this.setState({
+                 destination:showdata,
+                 rawdesdata:data
+             })
+
+        })
+
+
     }
     render() {
-        const title=<div>旅途<p className={"home-body-des-tips"}>沿途风景变化再辗转，人山人海的对白换一句等待。</p></div>
+        const title=<div>旅途<p className={"home-body-des-tips"}>沿途风景如歌变化再辗转，人山人海的对白换一句等待。</p></div>
         const content=function () {
             if(this.state.destination===''){
                 return <div className={"home-body-des"}>选择一个你要去的地方</div>
             }
             else{
-                return <div className={"home-body-des"}>{this.state.destination}</div>
+                let des=this.state.destination;
+                return <div className={"home-body-des"}><Deslist data={des}/></div>
             }
         }
         return (
