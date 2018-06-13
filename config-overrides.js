e6075554023d6c034ff0d043ae45e0d34bc797e5
Vijay <765713272@qq.com>
2018-06-13 19:17:19 +0800

    change theme of ant design

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,11 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess=require('react-app-rewire-less');
 module.exports = function override(config, env) {
-    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
+    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], config);
+    config = rewireLess.withLoaderOptions({
+        modifyVars:{
+            "@primary-color":"#6c848d"
+        }
+    })(config,env);
     return config;
 };
