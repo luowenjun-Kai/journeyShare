77fdd5ceb3cc92ced440853c13814a5910237dab
Vijay <765713272@qq.com>
2018-08-08 21:58:58 +0800

    update support page

--- src/components/Page/support/support.js
+++ src/components/Page/support/support.js
@@ -1,13 +1,18 @@
+import './support.css'
 import oops from '../../../assets/oops.png'
 import React,{ Component} from 'react';
 import Gapbar from '../../gapbar/gapbar';
+import Wave from '../../wave/wave';
 export default class SupportPage extends Component{
     render(){
         //以下为暂时性页面
-        const content=<div style={{textAlign:'center'}}><img alt={"oops"} src={oops}></img></div>
+        const content=<div style={{textAlign:'center'}}><Wave slogan={"敬请期待"}/></div>
+        const title=<div>
+            <p className={"slogan"}>"永远相信美好的事情即将发生"</p>
+        </div>
         return(
             <div>
-                <Gapbar title={"敬请期待"} content={content} style={{bkColor:'#fff',titleSize:'15vw'}}/>
+                <Gapbar title={content}  style={{bkColor:'#fff'}}/>
             </div>
         )
     }
